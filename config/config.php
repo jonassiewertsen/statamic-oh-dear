51f28fa81658650fbb6ad55c726ef9d4c9c14ede
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-01 00:38:25 +0100

    adding config

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_key' => env('OH_DEAR_API_KEY'),
+    'site_id' => env('OH_DEAR_SITE_ID'),
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -20,7 +20,7 @@ class ServiceProvider extends AddonServiceProvider
         parent::boot();
 
 //        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'howToAddon');
-//        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'howToAddon');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'oh-dear');
 //        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'howToAddon');
 //
 //        if ($this->app->runningInConsole()) {
@@ -28,9 +28,9 @@ class ServiceProvider extends AddonServiceProvider
 //                __DIR__.'/../resources/lang' => resource_path('lang/vendor/jonassiewertsen/howToAddon/'),
 //            ], 'How To Addon lang file');
 //
-//            $this->publishes([
-//                __DIR__ . '/../config/config.php' => config_path('how_to_addon.php'),
-//            ], 'How To Addon config file');
+            $this->publishes([
+                __DIR__ . '/../config/config.php' => config_path('statamic/oh-dear.php'),
+            ], 'Statamic Oh Dear config file');
 //        }
 //
         $this->bootNavigation();
