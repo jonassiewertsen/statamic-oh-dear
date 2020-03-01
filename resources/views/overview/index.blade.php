7b3a110523d36d98e55cc6d1181758fc7f60eb75
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-01 09:10:15 +0100

    Card design finished

--- resources/views/overview/index.blade.php
+++ resources/views/overview/index.blade.php
@@ -6,9 +6,37 @@
         <h1 class="flex-1">Oh Dear</h1>
     </div>
 
-    <div class="card">
-        @foreach($checks as $check)
-            {{ $check->type }}: {{ $check->latestRunResult }}<br>
-        @endforeach
-    </div>
+    @include('oh-dear::overview._card', [
+        'title' => 'Uptime',
+        'visual' => 'Site is up',
+        'text' => 'Your site is up. We last checked 2 Minutes ago.',
+        'link_url' => '#',
+        'link_text' => 'View the latest Report',
+    ])
+
+    @include('oh-dear::overview._card', [
+        'title' => 'Broken Links',
+        'visual' => 'None',
+        'text' => 'No broken links found. We last checked a day ago.',
+        'link_url' => '#',
+        'link_text' => 'View the latest Report',
+    ])
+
+    @include('oh-dear::overview._card', [
+        'title' => 'Mixed Content',
+        'visual' => 'Secure',
+        'text' => 'No mixed content found, your site is secure. We last checked a day ago.',
+        'link_url' => '#',
+        'link_text' => 'View the latest Report',
+    ])
+
+    @include('oh-dear::overview._card', [
+        'title' => 'Certificate health',
+        'visual' => 'Healty',
+        'text' => 'Your certificate is healthy. We last checked a minute ago.',
+        'link_url' => '#',
+        'link_text' => 'View the latest Report',
+    ])
+
+
 @endsection
