cdd181684e664eb10d1758827e2664d02ff2c147
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-01 09:26:09 +0100

    card texts now translateable

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -1,13 +1,17 @@
 <?php
 
 return [
-    'broken_links'       => 'Broken Links',
-    'certificate_health' => 'Certificate Healt',
-    'healthy'             => 'Healthy',
-    'mixed_content'      => 'Mixed Content',
-    'none'               => 'None',
-    'secure'             => 'Secure',
-    'site_up'            => 'Site is up',
-    'uptime'             => 'Uptime',
-    'view_latest_report' => 'View the latest Report',
+    'broken_links'                       => 'Broken Links',
+    'broken_links_success_message'       => 'No broken links found. We last checked :time.',
+    'certificate_health'                 => 'Certificate Healt',
+    'certificate_health_success_message' => 'Your certificate is healthy. We last checked :time.',
+    'healthy'                            => 'Healthy',
+    'mixed_content'                      => 'Mixed Content',
+    'mixed_content_success_message'      => 'No mixed content found, your site is secure. We last checked :time.',
+    'none'                               => 'None',
+    'secure'                             => 'Secure',
+    'site_up'                            => 'Site is up',
+    'uptime'                             => 'Uptime',
+    'uptime_success_message'             => 'Your site is up. We last checked :time.',
+    'view_latest_report'                 => 'View the latest Report',
 ];
--- resources/views/overview/index.blade.php
+++ resources/views/overview/index.blade.php
@@ -9,7 +9,7 @@
     @include('oh-dear::overview._card', [
         'title' => __('oh-dear::lang.uptime'),
         'visual' => __('oh-dear::lang.site_up'),
-        'text' => 'Your site is up. We last checked 2 Minutes ago.',
+        'text' => __('oh-dear::lang.uptime_success_message', ['time' => 'something']),
         'link_url' => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
@@ -17,7 +17,7 @@
     @include('oh-dear::overview._card', [
         'title' => __('oh-dear::lang.broken_links'),
         'visual' => __('oh-dear::lang.none'),
-        'text' => 'No broken links found. We last checked a day ago.',
+        'text' => __('oh-dear::lang.broken_links_success_message', ['time' => 'something']),
         'link_url' => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
@@ -25,7 +25,7 @@
     @include('oh-dear::overview._card', [
         'title' => __('oh-dear::lang.mixed_content'),
         'visual' => __('oh-dear::lang.secure'),
-        'text' => 'No mixed content found, your site is secure. We last checked a day ago.',
+        'text' => __('oh-dear::lang.mixed_content_success_message', ['time' => 'something']),
         'link_url' => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
@@ -33,7 +33,7 @@
     @include('oh-dear::overview._card', [
         'title' => 'Certificate health',
         'visual' => __('oh-dear::lang.healthy'),
-        'text' => 'Your certificate is healthy. We last checked a minute ago.',
+        'text' => __('oh-dear::lang.certificate_health_success_message', ['time' => 'something']),
         'link_url' => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
