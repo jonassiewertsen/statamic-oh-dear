1ad9c55635ee7ff4e43a99a756a3495e65f84bb5
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-01 09:26:48 +0100

    formatting

--- resources/views/overview/index.blade.php
+++ resources/views/overview/index.blade.php
@@ -7,34 +7,34 @@
     </div>
 
     @include('oh-dear::overview._card', [
-        'title' => __('oh-dear::lang.uptime'),
-        'visual' => __('oh-dear::lang.site_up'),
-        'text' => __('oh-dear::lang.uptime_success_message', ['time' => 'something']),
-        'link_url' => '#',
+        'title'     => __('oh-dear::lang.uptime'),
+        'visual'    => __('oh-dear::lang.site_up'),
+        'text'      => __('oh-dear::lang.uptime_success_message', ['time' => 'something']),
+        'link_url'  => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
 
     @include('oh-dear::overview._card', [
-        'title' => __('oh-dear::lang.broken_links'),
-        'visual' => __('oh-dear::lang.none'),
-        'text' => __('oh-dear::lang.broken_links_success_message', ['time' => 'something']),
-        'link_url' => '#',
+        'title'     => __('oh-dear::lang.broken_links'),
+        'visual'    => __('oh-dear::lang.none'),
+        'text'      => __('oh-dear::lang.broken_links_success_message', ['time' => 'something']),
+        'link_url'  => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
 
     @include('oh-dear::overview._card', [
-        'title' => __('oh-dear::lang.mixed_content'),
-        'visual' => __('oh-dear::lang.secure'),
-        'text' => __('oh-dear::lang.mixed_content_success_message', ['time' => 'something']),
-        'link_url' => '#',
+        'title'     => __('oh-dear::lang.mixed_content'),
+        'visual'    => __('oh-dear::lang.secure'),
+        'text'      => __('oh-dear::lang.mixed_content_success_message', ['time' => 'something']),
+        'link_url'  => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
 
     @include('oh-dear::overview._card', [
-        'title' => 'Certificate health',
-        'visual' => __('oh-dear::lang.healthy'),
-        'text' => __('oh-dear::lang.certificate_health_success_message', ['time' => 'something']),
-        'link_url' => '#',
+        'title'     => 'Certificate health',
+        'visual'    => __('oh-dear::lang.healthy'),
+        'text'      => __('oh-dear::lang.certificate_health_success_message', ['time' => 'something']),
+        'link_url'  => '#',
         'link_text' => __('oh-dear::lang.view_latest_report'),
     ])
 
