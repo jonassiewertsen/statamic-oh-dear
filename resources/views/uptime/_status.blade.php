1ab783655d1a401ab04f3d82e294423d1cbf0d4a
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-03 21:36:06 +0100

    uptime check

--- resources/views/uptime/_status.blade.php
+++ resources/views/uptime/_status.blade.php
@@ -1,4 +1,4 @@
-@if ($uptime['latest_run_result'] === 'success')
+@if ($uptime['latest_run_result'] === 'succeeded')
     @component('oh-dear::partials.info', ['type' => 'success'])
         <a href="{{ $url["href"] }}" target="_blank" class="underline">{{ $url["name"] }}</a> is up.
     @endcomponent
