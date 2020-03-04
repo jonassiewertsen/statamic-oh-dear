1c4e6670a5a37c867e5ae2f60cf29e83cd542e77
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-04 23:05:59 +0100

    done

--- src/Http/Controllers/UptimeController.php
+++ src/Http/Controllers/UptimeController.php
@@ -7,8 +7,7 @@ use Jonassiewertsen\OhDear\OhDear;
 class UptimeController {
     public function index() {
         $ohdear = new OhDear;
-
-        // TODO: Add the option, that checks can be disabled
+        
         $pastDays   = $ohdear->uptime(now()->subDays(6), now(), 'day');
         $pastMonths = $ohdear->uptime(now()->subMonths(11), now(), 'month');
         $downtimes  = $ohdear->downtime(now()->subMonths(5), now());
