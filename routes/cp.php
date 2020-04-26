ea6d1df67643b083173eca0167beb3c28728b6a8
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-26 19:40:08 +0200

    Routes compatible with Statamic beta 27

--- routes/cp.php
+++ routes/cp.php
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 */
 
-Route::prefix('oh-dear/')->name('oh-dear.')->namespace('Http\\Controllers\\')->group(function() {
+Route::prefix('oh-dear/')->name('oh-dear.')->group(function() {
     Route::get('/', 'OverviewController@index')->name('index');
     Route::get('/uptime', 'UptimeController@index')->name('uptime');
     Route::get('/broken-links', 'BrokenLinksController@index')->name('broken-links');
